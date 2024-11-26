5c3b5e877cd7bc4c1585d4ce871c0a1702ffc6c2
Xav86 <gustavo.g.2005@gmail.com>
2024-11-26 09:18:50 -0300

    feito adição das cidades e a destancia entre elas

--- algoritmo-de-Dijkstra.py
+++ algoritmo-de-Dijkstra.py
@@ -1,1 +1,28 @@
-# nada ainda
+import networkx as nx
+import numpy as np
+
+# Distancia entre as cidades
+distancias = [
+                [0, 4, 0, 0, 0, 0, 0, 8, 0   ],
+                [4, 0, 8, 0, 0, 0, 0, 11, 0  ],
+                [0, 8, 0, 7, 0, 4, 0, 0, 2  ],
+                [0, 0, 7, 0, 9, 14, 0, 0, 0  ],
+                [0, 0, 0, 9, 0, 10, 0, 0, 0  ],
+                [0, 0, 4, 14, 10, 0, 2, 0, 0],
+                [0, 0, 0, 0, 0, 2, 0, 1, 6   ],
+                [8, 11, 0, 0, 0, 0, 1, 0, 7  ],
+                [0, 0, 2, 0, 0, 0, 6, 7, 0   ]
+            ]
+
+# Lista das cidades
+cidades = {
+    0: "Criciúma",
+    1: "Içara",
+    2: "Nova Veneza",
+    3: "Cocal do Sul",
+    4: "Morro da Fumaça",
+    5: "Balneário Esplanada",
+    6: "Balneário Arroio Corrente",
+    7: "Sangão",
+    8: "Siderópolis"
+}
